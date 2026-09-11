4e0614c1c2b0be5959cc566f6ccb37cda2d87e21
viniciuss1812 <115712261+viniciuss1812@users.noreply.github.com>
2026-09-11 20:19:45 -0300

    bissecção

---
+++ estudo python/CalculoNumerico/main.py
@@ -0,0 +1,22 @@
+P = 35.000
+A =  8.500
+n = 7
+i = 0
+
+A = P*(i(1+i)**n/(1+i)**n -1)
+print = (A)
+
+
+def bisseccao ():
+    return 0 
+
+
+intervalo = [1,2]
+i = intervalo
+
+def bolzano (a,b):
+    if a*b< 0:
+        medio = a+b/
+
+
+
